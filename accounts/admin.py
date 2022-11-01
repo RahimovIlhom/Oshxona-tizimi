c0ed4d331004856ff18eb903279e3ecea762d02b
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2022-11-01 15:50:10 +0500

    registratsiya qisman bajarildi

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import EmployeeUserChangeForm, EmployeeUserCreateForm
+from .models import EmployeeUser
 
 # Register your models here.
+
+class EmployeeUserAdmin(UserAdmin):
+    add_form = EmployeeUserCreateForm
+    form = EmployeeUserChangeForm
+    model = EmployeeUser
+    list_display = ['username', 'first_name', 'last_name', 'email', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age', )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+
+admin.site.register(EmployeeUser, EmployeeUserAdmin)

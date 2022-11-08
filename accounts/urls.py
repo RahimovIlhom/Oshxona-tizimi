345849edaf376098e9eaa4059c5f7670f7d49afe
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2022-11-08 19:44:52 +0500

    users page create

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ChangeUserView, CreateUserView, AdminUserChangeView
+from .views import ChangeUserView, CreateUserView, AdminUserChangeView, all_users_view
 
 urlpatterns = [
     path('editing/<int:pk>/', ChangeUserView.as_view(), name='editing'),
     path('admin/edit/<int:pk>', AdminUserChangeView.as_view(), name='admin_edit'),
     path('adduser/', CreateUserView.as_view(), name='adduser'),
+    path('admin/users/', all_users_view, name='users'),
 ]

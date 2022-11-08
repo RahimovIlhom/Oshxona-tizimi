0eb8e0bc7b834bc61775cb05e40d88625d6c1d45
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2022-11-08 19:06:28 +0500

    add accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChangeUserView, CreateUserView
+from .views import ChangeUserView, CreateUserView, AdminUserChangeView
 
 urlpatterns = [
     path('editing/<int:pk>/', ChangeUserView.as_view(), name='editing'),
+    path('admin/edit/<int:pk>', AdminUserChangeView.as_view(), name='admin_edit'),
     path('adduser/', CreateUserView.as_view(), name='adduser'),
 ]

fe7e87247d55bb0eb4172031d371d29eb3d2f183
Ilhomjon Rahimov <94887912+RahimovIlhom@users.noreply.github.com>
2022-11-10 09:29:07 +0500

    time login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import EmployeeUser
 from .forms import EmployeeUserChangeForm, EmployeeUserCreateForm
+from django.contrib.auth import logout
+import datetime
+
+# from core.settings import SESSION_IDLE_TIMEOUT
+from django.conf import settings
+# import codesettings
 
 
 class ChangeUserView(UpdateView, LoginRequiredMixin):
@@ -20,6 +26,33 @@ class CreateUserView(CreateView, LoginRequiredMixin, UserPassesTestMixin):
 
     def test_func(self):
         return self.request.user.is_superuser
+
+
+
+class SessionIdleTimeout(object):
+    """Middle ware to ensure user gets logged out after defined period if inactvity."""
+
+    def __init__(self, get_response):
+        print("in init middleware")
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        if request.user.is_authenticated:
+            current_datetime = datetime.datetime.now()
+
+            if 'last_active_time' in request.session:
+                idle_period = (current_datetime -
+                               request.session['last_active_time']).seconds
+                if idle_period > settings.SESSION_IDLE_TIMEOUT:
+                    logout(request)
+                    response = redirect('/accounts/login/')
+                    response.delete_cookie('username')
+                    return response
+            request.session['last_active_time'] = current_datetime
+
+        response = self.get_response(request)
+        return response
 
 
 class AdminUserChangeView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
